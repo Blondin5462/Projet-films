3072a9964c03c1d10731f6f42175cdc6164485d8
Blondin5462 <maxime.leszczynski@gmail.com>
2022-02-05 08:15:01 +0100

    Refactorisation de l'application

--- src/App.js
+++ src/App.js
@@ -2,17 +2,31 @@ import React, { Component } from 'react';
 import { Header, MovieList, MovieDetails } from './components';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App d-flex flex-column">
-        <Header />
-        <div className="d-flex flex-row flex-fill pt-4 p-2" >
-          <MovieList />
-          <MovieDetails />
-        </div>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            movies: [{
+                img: '...',
+                title: 'The Godfather',
+                details: 'R | 175 min | Crime, Drama',
+                description: '...'
+            }],
+            selectedMovie: 0
+        }
+    }
+
+    render() {
+        return (
+            <div className="App d-flex flex-column">
+                <Header/>
+                <div className="d-flex flex-row flex-fill pt-4 p-2">
+                    <MovieList/>
+                    <MovieDetails/>
+                </div>
+            </div>
+        );
+    }
 }
+
 
 export default App;
